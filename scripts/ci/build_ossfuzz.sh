c4cb40891d90d7eaa98fc5a62fd61d3875ef3727
Mate Soos <mate.soos@argot.org>
2026-03-10 13:44:52 +0100

    Update

--- scripts/ci/build_ossfuzz.sh
+++ scripts/ci/build_ossfuzz.sh
@@ -25,10 +25,10 @@ function build_fuzzers
   mkdir -p "$CCACHE_DIR"
   # shellcheck disable=SC2086
   cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" \
-    -DCMAKE_TOOLCHAIN_FILE="${ROOTDIR}"/cmake/toolchains/libfuzzer.cmake
+    -DCMAKE_TOOLCHAIN_FILE="${ROOTDIR}"/cmake/toolchains/libfuzzer.cmake \
     $CMAKE_OPTIONS
   ccache -z
-  make ossfuzz ossfuzz_proto ossfuzz_abiv2 -j 4
+  make ossfuzz ossfuzz_proto ossfuzz_abiv2 -j 8
   ccache -s
 }
 
